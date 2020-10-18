ba61d8ae7c390e21062b07207b7ec86663271bf8
Jaishree Bala <jaishree1bala@gmail.com>
2020-10-18 02:45:30 -0400

    basic functionality

---
+++ results.js
@@ -0,0 +1,46 @@
+// local storage data
+gender = localStorage.getItem('gender');
+yob = localStorage.getItem('yob');
+ids = localStorage.getItem('symptomIds');
+gender = gender.substr(1, gender.length - 2)
+yob = yob.substr(1, yob.length - 2);
+
+const table = document.querySelector("table");
+async function fetchDataFromAPI(url) {
+    const data = await fetch(url, {
+        method: 'GET',
+    })
+    return data;
+}
+getResults();
+async function getResults() {
+    const fetchData = await fetchDataFromAPI(`https://healthservice.priaid.ch/diagnosis?symptoms=${ids}&gender=${gender}&year_of_birth=${yob}&token=${token}&format=json&language=en-gb`);
+    const results = await fetchData.json();
+    console.log(results);
+
+    results.forEach(result => {
+        console.log(result.Issue.Name);
+        let tableRow = document.createElement("tr");
+        let diseaseTd = document.createElement("td");
+        let accuracyTd = document.createElement("td");
+        let doctorSpecialization = document.createElement("td");
+        tableRow.appendChild(diseaseTd);
+        tableRow.appendChild(accuracyTd);
+        tableRow.appendChild(doctorSpecialization);
+        diseaseTd.innerText = result.Issue.Name;
+        accuracyTd.innerText = result.Issue.Accuracy + "%";
+
+
+        const docsList = result.Specialisation;
+        doctorSpecialization.innerText += `${docsList[0].Name} `
+        for (var i = 1; i < docsList.length; i++) {
+            doctorSpecialization.innerText += `, ${docsList[i].Name}`
+        }
+        // result.Specialisation.forEach(doctorType => {
+        //     console.log(doctorType.Name)
+        //     doctorSpecialization.innerText += `${doctorType.Name} `
+        // })
+        table.appendChild(tableRow)
+    });
+}
+
